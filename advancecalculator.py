ac4b3b4a4dfd6bbd66a8ad2ea35a867b5dc0e25f
sish11 <69053365+sish11@users.noreply.github.com>
2020-08-03 22:51:04 +0200

    Create advancecalculator.py

---
+++ advancecalculator.py
@@ -0,0 +1,16 @@
+ input("Operator: ")
+num1 = int(input("num1: "))= 
+
+if op == "+":num1 = int(input("num1: "))
+op =
+     print(num1 + num2)
+elif op == "-":
+     print(num1 - num2)
+elif op == "/":
+     print(num1 / num2)
+elif op == "*":
+     print(num1 * num2)
+else:
+     print("Invalid Operator")
+
+                
