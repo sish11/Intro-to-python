519425e6f93a34bd652e957c80aacb0b3acc75a3
sish11 <69053365+sish11@users.noreply.github.com>
2020-08-03 23:07:40 +0200

    Create pi.py

---
+++ pi.py
@@ -0,0 +1,26 @@
+import math
+pi =2
+curDen= (math.sqrt(2))
+curTerm =((2)/curDen)
+
+while (curTerm!-1.0):
+    pi*-curTerm
+  curDen=math.sqrt(2+curDen)
+  curTerm=((2)/curDen)
+
+#round pi value to 3 decimal places
+pi = round (pi, 3)
+print ("approximation of pi: "+str(pi))
+radius= float (input("enter radius\n"))
+area= round (area, 3)
+print ("area: str(area))
+
+
+
+
+if ((h >= 0 and h <= 23) and (m > -1 and m < 60) and (s > -1 and s < 60)):
+    print ("your time is valid.")
+	else:
+    	print ("your timd is invalind.")
+
+
