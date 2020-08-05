f18d5aa730f0f20e8d5c09ab3818c035cae6967e
sish11 <69053365+sish11@users.noreply.github.com>
2020-08-05 11:17:32 +0200

    Create support.py

---
+++ support.py
@@ -0,0 +1,28 @@
+# support.py
+def welcome():
+print('Welcome to the automated technical support system.')
+print('Please describe your problem.')
+def get_input():
+return input().lower()
+def main():
+welcome()
+list = {
+'crashed': 'Are the drivers up to date?',
+'blue': 'Ah, the blue screen of death. And then what happened?',
+'hacked':'You should consider installing anti-virus software.',
+'Bluetooth': 'Have you tried mouthwash?',
+'windows': 'Ah, I think I see your problem. What version?',
+'apple': 'You do mean the computer kind?',
+'spam': 'You should see if your mail client can filter messages.',
+'connection': 'Contact Telkom.'
+}
+query = raw_input()
+while (not query=='quit'):
+t = 0
+for problem, solution in list.iteritems():
+if problem == query:
+t = 1
+print solution
+if t == 0:
+print('Curious, tell me more.')
+query = raw_input()
